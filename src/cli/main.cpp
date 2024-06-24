381ff34680636adce2694d9116dfe25aadee37ce
xyb <xyb@xyb.name>
2024-06-24 10:16:08 -0700

    Add compile and run in main.scc, hellow_world works!

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,8 +1,13 @@
+#include <cassert>
+#include <filesystem>
 #include <format>
+#include <fstream>
 #include <iostream>
+#include <memory>
 #include <stdexcept>
 
 import scc.cli;
+import scc.compiler;
 
 struct Options {
     std::string inputFile {};
@@ -10,6 +15,8 @@ struct Options {
 };
 
 void PrintHelp(const std::string_view& optionsHelp);
+void CompileAndRun(const Options& options);
+std::shared_ptr<scc::compiler::AstScope> Parse(const std::string& file);
 
 int main(int argc, const char* const argv[])
 {
@@ -34,7 +41,7 @@ int main(int argc, const char* const argv[])
         }
 
         options.inputFile = std::move(cmdProcessor.GetArgs().front());
-        std::cout << "TODO: process input file: " << options.inputFile << std::endl;
+        CompileAndRun(options);
         return 0;
     } catch (const std::exception& ex) {
         std::cerr << "scc: \e[91merror:\e[0m " << ex.what() << std::endl;
@@ -49,4 +56,40 @@ void PrintHelp(const std::string_view& optionsHelp)
               << std::endl
               << optionsHelp << std::endl
               << std::endl;
+}
+
+void CompileAndRun(const Options& options)
+{
+    assert(!options.inputFile.empty());
+
+    // Parse.
+    auto scope = Parse(options.inputFile);
+
+    // Translate.
+    auto filePath = std::filesystem::path { options.inputFile };
+    auto workingFolder = filePath.parent_path() / ".scc";
+    std::filesystem::create_directories(workingFolder);
+
+    auto outFile = workingFolder / (filePath.filename().string() + ".cpp");
+    scc::compiler::Translator { std::make_shared<std::ofstream>(outFile) }.VisitAstScope(*scope);
+
+    // Invoke clang++ to compile.
+    char result[PATH_MAX];
+    result[readlink("/proc/self/exe", result, PATH_MAX)] = '\0';
+    auto appPath = std::filesystem::path { result }.parent_path();
+    auto stdModulePath = appPath / "std";
+    auto stdLibPath = stdModulePath / "libscc.std.a";
+    auto exePath = workingFolder / "a.out";
+    std::system(std::format("clang++-18 -std=c++20 -fprebuilt-module-path={} -w {} {} -o {}", stdModulePath.string(), outFile.string(), stdLibPath.string(), exePath.string()).c_str());
+
+    // Run.
+    std::system(exePath.string().c_str());
+}
+
+std::shared_ptr<scc::compiler::AstScope> Parse(const std::string& file)
+{
+    auto scope = std::make_shared<scc::compiler::AstScope>();
+    scc::compiler::Lexer lexer { std::make_shared<std::ifstream>(file) };
+    scc::compiler::Parser {}.ParseCompileUnit(scope, lexer);
+    return std::move(scope);
 }
---
+++ src/std/print/println.cpp
@@ -0,0 +1,15 @@
+module;
+
+#include <format>
+
+export module scc.std:println;
+
+namespace scc::std {
+
+export template <class... Args>
+void println(const ::std::string_view& fmt, Args&&... args)
+{
+    printf("%s\n", ::std::vformat(fmt, ::std::make_format_args(args...)).c_str());
+}
+
+}
