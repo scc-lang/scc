aa3bad9694dd62310ef489e3b97664071bd0a0eb
xyb <xyb@xyb.name>
2024-06-26 06:53:47 -0700

    Improve error report

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <memory>
 #include <stdexcept>
+#include <unistd.h>
 
 import scc.cli;
 import scc.compiler;
@@ -18,11 +19,13 @@ struct Options {
 void PrintHelp(const std::string_view& optionsHelp);
 void CompileAndRun(const Options& options);
 scc::compiler::AstScope Parse(const std::string& file);
+std::string GetFileLine(const std::string& file, int line);
+bool IsErrorColorSupported();
 
 int main(int argc, const char* const argv[])
 {
+    Options options {};
     try {
-        Options options {};
         scc::cli::CommandlineProcessor cmdProcessor {};
         cmdProcessor.RegisterOption('h', "help", "Print help", [&options] { options.needHelp = true; });
         cmdProcessor.RegisterOption('c', "Compile only", [&options] { options.compileOnly = true; });
@@ -45,8 +48,32 @@ int main(int argc, const char* const argv[])
         options.inputFile = std::move(cmdProcessor.GetArgs().front());
         CompileAndRun(options);
         return 0;
+    } catch (const scc::compiler::Exception& ex) {
+        bool hasColor = IsErrorColorSupported();
+        std::string highlightTextColor = hasColor ? "\e[97m" : "";
+        std::string highlightErrorColor = hasColor ? "\e[91m" : "";
+        std::string turnOffColor = hasColor ? "\e[0m" : "";
+
+        if (options.inputFile.empty()) {
+            std::cerr << "scc";
+        } else {
+            std::cerr << highlightTextColor << options.inputFile << ":" << ex.startLine << ":" << ex.startColumn;
+        }
+        std::cerr << ": " << highlightErrorColor << "error: " << highlightTextColor << ex.what() << turnOffColor << std::endl;
+
+        auto prefix = std::format("{:5} | ", ex.startLine);
+        auto line = GetFileLine(options.inputFile, ex.startLine);
+        std::cerr << prefix << line << std::endl;
+        std::cerr << std::string(prefix.length() + ex.startColumn - 1, ' ')
+                  << highlightErrorColor << std::string((ex.startLine == ex.endLine ? ex.endColumn : line.length() - 1) - ex.startColumn + 1, '^') << turnOffColor
+                  << std::endl;
+        return 1;
     } catch (const std::exception& ex) {
-        std::cerr << "scc: \e[91merror:\e[0m " << ex.what() << std::endl;
+        if (IsErrorColorSupported()) {
+            std::cerr << "scc: \e[91merror:\e[0m " << ex.what() << std::endl;
+        } else {
+            std::cerr << "scc: error: " << ex.what() << std::endl;
+        }
         return 1;
     }
 }
@@ -96,4 +123,21 @@ scc::compiler::AstScope Parse(const std::string& file)
     scc::compiler::Lexer lexer { std::make_shared<std::ifstream>(file) };
     scc::compiler::Parser {}.ParseCompileUnit(scope, lexer);
     return std::move(scope);
+}
+
+std::string GetFileLine(const std::string& file, int line)
+{
+    std::ifstream in { file };
+    std::string str {};
+    for (int i = 0; i < line; ++i) {
+        if (std::getline(in, str).eof()) {
+            return "";
+        }
+    }
+    return str;
+}
+
+bool IsErrorColorSupported()
+{
+    return isatty(STDERR_FILENO);
 }
